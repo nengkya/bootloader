291666d9656f2adf272a81aa84323d5694b14946
nengkya <nengkya@yahoo.com>
2023-08-25 06:41:07 +0700

    .fill 510-(.-init), 1, 0 # add zeroes to make it 510 bytes long

--- boot.s
+++ boot.s
@@ -1,7 +1,9 @@
-.code64 #tell the assembler that we are using 64 bit mode.
+.code64 	 #tell the assembler that we are using 64 bit mode.
 .global init #makes "init" label available to the outside.
 			 #the standard would be "_start",
 			 #chose "init" to illustrate that we can call it anything.
 
 init:
 	jmp init
+
+.fill 510 - (.-init), 1, 0	#add zeroes to make it 510 bytes long
