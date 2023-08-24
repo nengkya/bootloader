5cf06bd3e931e29f3c88ff99691f8d189387fafc
nengkya <nengkya@yahoo.com>
2023-08-25 06:20:42 +0700

    .global init

---
+++ boot.s
@@ -0,0 +1,4 @@
+.code64 #tell the assembler that we are using 64 bit mode.
+.global init #makes "init" label available to the outside.
+			 #the standard would be "_start",
+			 #chose "init" to illustrate that we can call it anything.
