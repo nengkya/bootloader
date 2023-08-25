b6c2454dfe00249ddffbc136d34db9595eb858d3
nengkya <nengkya@yahoo.com>
2023-08-25 11:03:18 +0700

    doesnt work with .code64, must use .code16 bit

--- boot.s
+++ boot.s
@@ -1,13 +1,12 @@
-.code64 	 #tell the assembler that we are using 64 bit mode.
-.global init #makes "init" label available to the outside.
-			 #the standard would be "_start",
-			 #chose "init" to illustrate that we can call it anything.
+.code16			#tell the assembler that we are using 16 bit mode.
+.global init 	#makes "init" label available to the outside.
+			 	#the standard would be "_start",
+			 	#chose "init" to illustrate that we can call it anything.
 
 init:
-	mov $0x0e41, %ax		#sets AH to 0xe (function teletype/write to screen).
-							#sets AL to 0x41 (ascii "A").
+	mov $0x0e41, %ax 	#sets AH to 0xe (function teletype) and al to 0x41 (ASCII "A").
 	int $0x10
-	hlt						#halt until the next external interrupt is fired.
+	hlt 				#halt, waiting next interrupt.
 
 .fill 510 - (.-init), 1, 0	#add zeroes to make it 510 bytes long.
 .word 0xaa55				#magic bytes that tell BIOS that this is bootable.
