19084601dc99b51a1cf8879484358dc2ccccb727
nengkya <nengkya@yahoo.com>
2023-08-25 07:54:26 +0700

    ljpmw bashxffff, bash #jump to reset vector, physical address FFFF:0000

--- boot.s
+++ boot.s
@@ -4,6 +4,9 @@
 			 #chose "init" to illustrate that we can call it anything.
 
 init:
-	jmp init
+	ljpmw $0xffff, $0		#jump to the "reset vector", doing a warm reboot
+							#physical address FFFF:0000
+							#https://en.wikipedia.org/wiki/Reset_vector
 
 .fill 510 - (.-init), 1, 0	#add zeroes to make it 510 bytes long
+.word 0xaa55				#magic bytes that tell BIOS that this is bootable
