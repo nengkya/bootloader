06a05397520dd5c143e0cfdd18a50f0f3c88b678
nengkya <nengkya@yahoo.com>
2025-10-03 15:48:22 +0700

    #jmp, ljmp only supported in .code16 bit real mode

--- boot.s
+++ boot.s
@@ -1,13 +1,14 @@
 #.code32 or .code64         doesnt work booting, wont print A
 .code16                     #tell the assembler that we are using 16 bit real mode
-.global init                #makes "init" label available to the outside
+                            #jmp, ljmp only supported in .code16 bit real mode
+#.global init               #makes "init" label available to the outside
                             #the standard would be "_start",
                             #chose "init" to illustrate that we can call it anything
 
 init:
     ljmp  $0x07C0, $9       #bootloader internal jump
-    ljmpw $0xf000, $0xfff0  #CPU reset/BIOS entry point
     ljmp  $0xf000, $0xe05b  #call BIOS internal function
+    ljmpw $0xf000, $0xfff0  #CPU reset/BIOS entry point
      jmp  $0xFFFF, $0x0000  #CPU reset entry point
     ljmpw $0xffff, $1       #wont boot
     ljmpw $0xffff, $0       #segment:offset
@@ -16,5 +17,5 @@ init:
 	hlt                     #halt, waiting next interrupt
 
 #syntax .fill, count, size, value
-.fill 510 - (.-init), 1, 0	#add zeroes to make it 510 bytes long
+.fill 510 - (.-init), 1, 10	#add zeroes to make it 510 bytes long
 .word 0xaa55                #magic bytes that tell BIOS that this is bootable
--- hello.s
+++ hello.s
@@ -1,15 +1,15 @@
 .global _start
 
 _start:
-	mov $1, %rax	#system call 1 is write
-	mov $1, %rdi	#file handle 1 is stdout
-					#destination index register
+	mov $1, %rax	    #system call 1 is write
+	mov $1, %rdi	    #file handle 1 is stdout
+					    #destination index register
 	mov $message, %rsi	#address of string
 						#source index register
-	mov $13, %rdx	#number of bytes
+	mov $13, %rdx	    #number of bytes
 	syscall
 
-	mov $60, %rax #system call 60 is exit
+	mov $60, %rax       #system call 60 is exit
 	xor %rdi, %rdi
 	syscall
 
